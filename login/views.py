8fe1fa598010a3db049580db6a98c4181d154432
Artemka-dev <artemka_pro@inbox.ru>
2019-12-31 18:46:53 +0300

    Mod Django email

--- login/views.py
+++ login/views.py
@@ -74,9 +74,9 @@ class LoginUser(View):
 				email = EmailMessage(
 					subject="От администрации django",
 					body=f"Был выполнен вход на ваш аккаунт - {client_ip}",
-					from_email="django_email@python.ru",
+					from_email="django_admin@python.ru",
 					to=[user.email],
-					reply_to=[user.email]
+					reply_to=[user.email, "django_admin@python.ru"]
 				)
 				sent = email.send(fail_silently=False)
 			except Exception:
